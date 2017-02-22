d5e23ac9018d1305c75a9dec579b450a22083a80
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-02-22 12:52:33 +0200

    Tests added

---
+++ test/Functional/StackOverflowTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Test\Functional;
+
+require_once(__DIR__ . '/../../src/Models/checkRequest.php');
+class StackOverflowTest extends BaseTestCase {
+
+    public function testListMetrics() {
+
+        $routes = [
+            'createFilter',
+            'getMyReputationHistory',
+            'getUsersReputation',
+            'getMyReputation',
+            'getUserPrivileges',
+            'getMyPrivileges',
+            'getAllPrivileges',
+            'getPostsByUserIds',
+            'getMyPosts',
+            'getPostsByIds',
+            'getAllPosts',
+            'getUserUnreadNotifications',
+            'getMyUnreadNotifications',
+            'getUserNotifications',
+            'getMyNotifications',
+            'getMyActivity',
+            'getUserActivity',
+            'getAssociatedAccountsByUserIds',
+            'getMyAssociatedAccounts',
+            'getUserUnreadInboxItems',
+            'getMyUnreadInboxItems',
+            'getUserInbox',
+            'getMyInbox',
+            'getStackoverflowStatistics',
+            'getAllEvents',
+            'getMentionedUsersComments',
+            'getMentionedMeComments',
+            'getUsersReplyComments',
+            'getMyReplyComments',
+            'getUsersComments',
+            'getMyComments',
+            'getQuestionsComments',
+            'getPostsCommentsByIds',
+            'deleteSingleComment',
+            'undoUpvoteOnComment',
+            'upvoteComment',
+            'addFlagToComment',
+            'getQuestionFlagOptions',
+            'getCommentFlagOptions',
+            'updateSingleComment',
+            'createComment',
+            'getCommentsByIds',
+            'getAllComments',
+            'getAnswersComments',
+            'deleteSingleAnswer',
+            'getUsersBadges',
+            'getMyBadges',
+            'getTagsAwardedBadges',
+            'getAllNamedBadges',
+            'getRecentlyAwardedBadges',
+            'getBadgesByIds',
+            'getAllBadges',
+            'getUserTopAnswersByTags',
+            'getMyTopAnswersByTags',
+            'getAnswersByUserIds',
+            'getMyAnswers',
+            'createAnswerOnQuestion',
+            'getAnswersToQuestions',
+            'addFlagToAnswer',
+            'getAnswerFlagOptions',
+            'updateSingleAnswer',
+            'undoUpvoteOnAnswer',
+            'upvoteAnswer',
+            'undoDownvoteOnAnswer',
+            'downvoteAnswer',
+            'undoAcceptOnAnswer',
+            'acceptAnswer',
+            'getAnswersByIds',
+            'getAllAnswers',
+            'getUserTimeline',
+            'getMyTimeline',
+            'getElectedModerators',
+            'getModerators',
+            'getUsers',
+            'getUsersByIds',
+            'getMe',
+            'deAuthenticateApplication',
+            'invalidateAccessToken',
+            'getAccessToken'
+
+        ];
+
+        foreach($routes as $file) {
+            $var = '{  
+                    }';
+            $post_data = json_decode($var, true);
+
+            $response = $this->runApp('POST', '/api/StackOverflow/'.$file, $post_data);
+
+            $this->assertEquals(200, $response->getStatusCode(), 'Error in '.$file.' method');
+        }
+    }
+
+}
